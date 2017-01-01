634415ed6f3481a55c2f8bf92b2c62a9d7706531
Linus Birgerstam <linus@birgerstam.se>
2017-01-01 18:26:12 +0100

    Updated assembly description

--- lib/cs/src/EmojiOne/Properties/AssemblyInfo.cs
+++ lib/cs/src/EmojiOne/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EmojiOne")]
-[assembly: AssemblyDescription("A lib for converting Unicode emoji characters to :shortnames: and :shortnames: to emoji images etc.")]
+[assembly: AssemblyDescription("A C# implementation of the EmojiOne lib for converting Unicode emoji characters to :shortnames: and :shortnames: to emoji images etc.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EmojiOne")]
